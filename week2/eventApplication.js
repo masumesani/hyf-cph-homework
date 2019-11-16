2061d66fc43f23d0a928443b6b615feebfdb0ca0
Masume <masume.sani@gmail.com>
2019-11-16 18:15:52 +0100

    Fixes #2 : change the function to cover math of exersize

--- week2/eventApplication.js
+++ week2/eventApplication.js
@@ -1,16 +1,9 @@
-function getWeekDay(date) {
-    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = date.getDay();
-    return weekdays[day];  
+function getEventWeekDay(daysUpToEvent) {
+    let daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    return `Today is ${new Date()} . Your next event will be held on ${daysOfTheWeek[(daysUpToEvent + new Date().getDay()) % (daysOfTheWeek.length)]} `;
 }
-
-let date = new Date();
-let weekDay = getWeekDay(date);
-
-let eventDate = new Date('November 24, 2019');
-let eventDay = getWeekDay(eventDate);
-
-let countDays = Math.ceil((eventDate - date)/(1000*60*60*24))
-
-console.log(`Today is ${weekDay} and the event is in ${countDays} days. The event will be held on ${eventDay}`);
-
+console.log(getEventWeekDay(4));
+console.log(getEventWeekDay(6));
+console.log(getEventWeekDay(0));
+console.log(getEventWeekDay(21));
+console.log(getEventWeekDay(2));
