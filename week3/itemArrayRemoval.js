b454951169e6aeaa5abc7e58c187794b4a08af96
Masume <masume.sani@gmail.com>
2019-11-22 22:47:55 +0100

    third week js1 homework

---
+++ week3/itemArrayRemoval.js
@@ -0,0 +1,12 @@
+let names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
+let nameToRemove = 'Ahmad';
+
+// Write some code here
+let filteredItems = names.filter(item => item !== nameToRemove);
+console.log(filteredItems); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+let items = ['a', 'b', 'c', 'd', 'e', 'f'];
+let valueToRemove = 'c';
+filteredItems = items.filter(item => item !== valueToRemove);
+console.log(filteredItems); // ['a', 'b','d', 'e', 'f']
+ 
+// Code done
